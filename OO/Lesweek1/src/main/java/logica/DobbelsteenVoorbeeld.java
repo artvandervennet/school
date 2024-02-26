c0227f5becb9046fa0b0650f56f9c12870a107f5
vandervennetart <art.van.der.vennet.be@gmail.com>
2024-02-26 15:05:45 +0100

    les1 OO

---
+++ OO/Lesweek1/src/main/java/logica/DobbelsteenVoorbeeld.java
@@ -0,0 +1,21 @@
+package logica;
+
+public class DobbelsteenVoorbeeld {
+
+
+    public DobbelsteenVoorbeeld(int min, int max){
+
+    }
+
+    public void gooi(){
+    }
+
+    public int get() {
+        return ;
+    }
+
+    @Override
+    public String toString() {
+        return ;
+    }
+}
---
+++ OO/Lesweek1/src/main/java/presentatie/DemoDobbelsteenVoorbeeld.java
@@ -0,0 +1,13 @@
+package presentatie;
+
+import logica.DobbelsteenVoorbeeld;
+
+public class DemoDobbelsteenVoorbeeld {
+    public static void main(String[] args) {
+        DobbelsteenVoorbeeld dobbel = new DobbelsteenVoorbeeld(0,6);
+        dobbel.gooi();
+        dobbel.get();
+    }
+
+
+}
