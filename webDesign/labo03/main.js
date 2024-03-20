080f0aa2f54946507943cc03e33343eff4b72c67
vandervennetart <art.van.der.vennet.be@gmail.com>
2024-03-20 16:09:59 +0100

    web labo03 js

--- webDesign/labo03/main.js
+++ webDesign/labo03/main.js
@@ -1,5 +1,34 @@
 (function () {
+    const menuBtn = document.querySelector("#menu");
+    const showMoreBtn = document.querySelector("#showMore");
+
+
+    const menu = document.querySelector("header nav");
+    const morePictures = document.querySelectorAll(
+        ".gallery img:not(img:first-child)"
+    );
+
+
+    let menuHidden = false;
+
+    menuBtn.addEventListener("click", function () {
+        console.log("a");
+
+        menuHidden = !menuHidden;
+        menu.hidden = menuHidden;
+    });
+
+    showMoreBtn.addEventListener("click", function () {
+        console.log("a");
+
+        menuHidden = !menuHidden;
+        menu.computedStyleMap.di
+    });
+
 
     
+    
+    menuBtn.click();
+    showMoreBtn.click();
 
-})
+})();
