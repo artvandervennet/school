0c64a9aaa89be70136c215e27d9dd1a13844ebbd
vandervennetart <art.van.der.vennet.be@gmail.com>
2024-03-24 13:28:29 +0100

    added menu button

--- webDesign/labo03/main.js
+++ webDesign/labo03/main.js
@@ -1,26 +1,19 @@
 (function () {
     const menuBtn = document.querySelector("#menu");
-    const showMoreBtn = document.querySelector("#showMore");
 
-    const menu = document.querySelector("header nav");
+    const showMoreBtn = document.querySelector("#showMore");
     const morePictures = document.querySelectorAll(
         ".gallery img:not(img:first-child)"
     );
     const showMoreTekst = document.querySelector("#more");
     const showLessTekst = document.querySelector("#less");
 
-    let menuHidden = true;
     let picHidden = true;
 
     menuBtn.addEventListener("click", function () {
-        console.log("a");
-
-        menu.className = menuHidden ? "visableMenu" : "";
-        menuHidden = !menuHidden;
+        menuBtn.ariaExpanded = !(menuBtn.ariaExpanded === "true");
     });
 
-
-    
     showMoreBtn.addEventListener("click", function () {
         morePictures.forEach((element) => {
             console.log(element);
@@ -32,6 +25,5 @@
         picHidden = !picHidden;
     });
 
-    menuBtn.click();
     showMoreBtn.click();
 })();
